4bae57113285076df3b16461a550eb616b23848f
weilan.tao <twlemily@hotmail.com>
2020-12-29 23:07:09 +0800

    userauth page v1

--- front/datecountdown/src/pages/UserAuth.vue
+++ front/datecountdown/src/pages/UserAuth.vue
@@ -1,18 +1,36 @@
 <template>
-    <form>
-        <div class="form-control">
-            <label for="email">Account/E-Mail</label>
-            <input type="email" id="email" placeholder="Please enter your email...">
-        </div>
-        <div class="form-control">
-            <label for="password">Password</label>
-            <input type="password" id="password" placeholder="Please enter your password...">
-        </div>
-        <button>Login</button>
-        <button mode="flat">Signup</button>
-    </form>
-</template>
+<body>
+        <h1>Date Count Down</h1>
 
+        <table>
+            <tr>
+                <td class="label">
+                    <label for="email">Account/Eail:</label>
+                </td>
+                <td class="input">
+                    <input type="email" id="email" placeholder="Please enter your email...">
+                </td>        
+            </tr>
+            <tr>
+                 <td class="label">
+                    <label for="password">Password:</label>
+                </td>
+                <td class="input">
+                    <input type="password" id="password" placeholder="Please enter your password...">
+                </td> 
+            </tr>
+            <tr>
+                <td >
+                </td>
+                <td class="buttons">
+                      <button>Login</button>
+                    <button >Signup</button>
+                </td>
+            </tr>
+        </table>
+
+</body>
+</template>
 
 <script>
 export default {
@@ -21,18 +39,66 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Spectral:wght@200&display=swap');
 
-form{
-    background:linear-gradient(rgb(126, 126, 226),rgb(47, 59, 114))
+*{
+    margin:0;
+    padding:0;
+}
+
+body{
+        /* background:black; */
+
+    background:linear-gradient(rgb(126, 126, 226),rgb(47, 59, 114));
+    /* justify-content:center; */
+    position: relative;
+}
+h1{
+ font-size:80px;
+ color:white;
+}
+
+table{
+    /* height:30% ; */
+    /* width:350px; */
+    margin:70%,50%,30%,50%;
+    position: relative;
+    border:1px solid#000;
+}
+td.label{
+    width:20%; 
+    border:6px ;
+}
+td.input{
+    width:80%;
+    border:6px solid rgba(0,0,0,0) ;
+}
+td.buttons{
+    border:6px solid rgba(0,0,0,0) ;
+    display:flex;
+    justify-content:space-evenly;
+    
+}
+button{
+border-radius: 10px;
+border-color:yellow;
+}
+
+
+label, button{
+    color:coral;
+}
+
+label{
+    float:right;
 }
 
 input {
     display: block;
-    width:30%;
-    font-family: 'Spectral', serif;
+    width:90%;
+    border-radius: 8px;
     border: 1px solid rgb(9, 99, 233);
     padding: 0.15rem;
+    
 }
 
 input:focus{
