c12c1e81c1043b04e6e679482f216317bd50999b
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-01-02 13:42:35 +0800

    test 1

---
+++ back/datecountdown/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+d
+4src/main/java/com/wlt/datecountdown/entity/User.java,7\3\73adb5a7fe1b28be605e1326a78269064716270f
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ back/datecountdown/target/classes/com/wlt/datecountdown/DatecountdownApplication.java
@@ -0,0 +1,13 @@
+package com.wlt.datecountdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatecountdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatecountdownApplication.class, args);
+	}
+
+}
